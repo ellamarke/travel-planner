5bc27bb7cd669efe67a7b25f5e28d535b49416d4
Ella Marke <ella.me.marke@gmail.com>
2021-02-07 11:27:14 +0000

    Components wireframed

--- src/HomeSearch.js
+++ src/HomeSearch.js
@@ -4,7 +4,7 @@ import "./css/Layout.css";
 function HomeSearch() {
   return (
     <div className="home-search">
-      <h2>find somewhere new</h2>
+      <h2>somewhere in mind?</h2>
       <h1>Where do you want to go next?</h1>
       <input
         className="search-bar-main"
--- src/WhereNext.js
+++ src/WhereNext.js
@@ -4,16 +4,19 @@ import "./css/Layout.css";
 function WhereNext() {
   return (
     <div className="where-next">
-      <h1>I want to see</h1>
-      <div className="place-dropdown">
-        <button className="dropdown-button">
-          <h1>Great Lakes</h1>
-        </button>
-        <div className="dropdown-content">
-          <a href="#">Misty Mountains</a>
-          <a href="#">Crumbling Temples</a>
-          <a href="#">Dark Jungles</a>
-          <a href="#">Desert Cities</a>
+      <h2>Find somewhere new</h2>
+      <div className="dropdown-sentence">
+        <h1>I want to see</h1>
+        <div className="place-dropdown">
+          <button className="dropdown-button">
+            <h1>Great Lakes</h1>
+          </button>
+          <div className="dropdown-content">
+            <a href="#">Misty Mountains</a>
+            <a href="#">Crumbling Temples</a>
+            <a href="#">Dark Jungles</a>
+            <a href="#">Desert Cities</a>
+          </div>
         </div>
       </div>
       <button className="go">Let's explore!</button>
