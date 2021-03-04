0ac379753db907094def58e4816e5efbd747ee0a
Ella Marke <ella.me.marke@gmail.com>
2021-03-04 09:32:06 +0000

    Floating surfers

--- src/HomePage/Hero.js
+++ src/HomePage/Hero.js
@@ -20,8 +20,29 @@ function Hero() {
         <p className="place-tag">Sydney, Australia</p>
 
         <img
-          src="img/home/surfers.png"
-          className="hero-image-surfers"
+          src="img/home/surfer-1.png"
+          className="hero-image-surfers surfer-4"
+          alt="the ocean in sydney, australia"
+          id="surfers"
+        ></img>
+
+        <img
+          src="img/home/surfer-2.png"
+          className="hero-image-surfers surfer-1"
+          alt="the ocean in sydney, australia"
+          id="surfers"
+        ></img>
+
+        <img
+          src="img/home/surfer-3.png"
+          className="hero-image-surfers surfer-2"
+          alt="the ocean in sydney, australia"
+          id="surfers"
+        ></img>
+
+        <img
+          src="img/home/surfer-4.png"
+          className="hero-image-surfers surfer-3"
           alt="the ocean in sydney, australia"
           id="surfers"
         ></img>
