6411771c3fed24342880001646974442181ead15
Ella Marke <ella.me.marke@gmail.com>
2021-03-13 13:15:23 +0000

    Updated starter lists

--- src/Lists/StarterLists.js
+++ src/Lists/StarterLists.js
@@ -1,18 +1,30 @@
 const initialLists = [
   {
-    listName: "Master Packing List",
+    listName: "Things to do in Tokyo",
     items: [
-      { text: "Shirts", isComplete: false, id: 1 },
-      { text: "Trousers", isComplete: false, id: 2 },
-      { text: "Passport", isComplete: false, id: 3 },
+      { text: "Harajuku", isComplete: false, id: 1 },
+      { text: "Tokyo Tower", isComplete: true, id: 2 },
+      { text: "Nogi Shrine", isComplete: false, id: 3 },
+      { text: "Eat at Yanaka Ginza", isComplete: false, id: 4 },
+      { text: "Walk around Shibuya", isComplete: true, id: 5 },
+      { text: "Nezu Museum", isComplete: false, id: 6 },
     ],
   },
   {
     listName: "Weekend Away Packing List",
     items: [
-      { text: "Skirts", isComplete: false, id: 1 },
-      { text: "Glasses", isComplete: true, id: 2 },
-      { text: "Tickets", isComplete: false, id: 3 },
+      { text: "Passport", isComplete: true, id: 1 },
+      { text: "Boarding Pass", isComplete: true, id: 2 },
+      { text: "Sunglasses", isComplete: false, id: 3 },
+      { text: "Underwear", isComplete: false, id: 4 },
+      { text: "Two pairs of trousers", isComplete: false, id: 5 },
+      { text: "Three tops", isComplete: false, id: 6 },
+      { text: "Day bag", isComplete: false, id: 7 },
+      { text: "Suncream", isComplete: false, id: 8 },
+      { text: "Guidebook", isComplete: false, id: 9 },
+      { text: "Phone charger", isComplete: false, id: 10 },
+      { text: "Toiletries", isComplete: false, id: 11 },
+      { text: "Pyjamas", isComplete: false, id: 12 },
     ],
   },
 ];
