530bab6cbb42fb23d0390a2b1cbae3a7aa387128
Ella Marke <ella.me.marke@gmail.com>
2021-03-22 15:19:32 +0000

    Read me and starter list updates

--- src/Lists/StarterLists.js
+++ src/Lists/StarterLists.js
@@ -34,10 +34,10 @@ export default initialLists;
 export const newList = {
   listName: "New List",
   items: [
-    /*     {
-      text: "Skirts",
+    {
+      text: "New Item",
       isComplete: false,
       id: Math.floor(Math.random() * 10000),
-    }, */
+    },
   ],
 };
